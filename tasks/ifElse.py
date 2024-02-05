29478a067d1b34043d9b5784a78171836f20724e
Rastsislau Rudziankou <rudenkovrostislav1@gmail.com>
2024-02-05 22:03:05 +0100

    task elseIf

---
+++ tasks/ifElse.py
@@ -0,0 +1,85 @@
+# task 1
+
+a = 10
+if a > 10:
+    print("The value greater than 10")
+elif a < 10:
+    print("The value less than 10")
+else:
+    print("The value equal 10")
+
+# task 2
+
+b = 50
+if b > 0 and b < 10 or b > 100 and b < 1000:
+    print(b * b)
+else:
+    print(b * 2)
+
+# task 3
+
+c = 10
+if c > 0:
+    print("The value is positive")
+elif c < 0:
+    print("The value is negative")
+else:
+    print("The value is equal 0")
+
+# task 4
+
+d = 19
+if d % 2 == 0:
+    print("The value is even")
+else:
+    print("The value is odd")
+
+# task 5
+
+e = 100
+if e >= 10 and e < 100:
+    print("The value is two-digit number")
+else:
+    print("The value is not two-digit number")
+
+# task 6
+
+f = 101
+if f >= -100 and f <= 100:
+    print("The value is range of numbers between -100 and 100")
+else:
+    print("The value is not range of numbers between -100 and 100")
+
+# task 7
+
+g = 12
+if g % 3 == 0 and g % 5 == 0:
+    print("The value is divides on 3 and 5")
+else:
+    print("The value is not divides on 3 and 5")
+
+# task 8
+
+h = 13
+if h % 2 == 0 and g % 7 == 0:
+    print("The value is divides on 2 and 7")
+else:
+    print("The value is not divides on 2 and 7")
+
+# task 9
+
+j = -8
+if j < 0:
+    print(j * -1)
+else:
+    print(j * -1)
+
+# task 10
+
+k = 999
+if k > 1000:
+    print(k / 10)
+else:
+    print("The value is less than 1000 or equal 1000")
+
+
