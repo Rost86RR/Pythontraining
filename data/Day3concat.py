79ee3315226e749d0bb72417c3bb32a6bb2e9860
Rastsislau Rudziankou <rudenkovrostislav1@gmail.com>
2024-11-06 16:01:45 +0100

    data day3 concat

---
+++ data/Day3concat.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+# Первый DataFrame с информацией о продуктах
+data1 = pd.DataFrame({
+    'product_id': [1, 2, 3],
+    'product_name': ['Widget A', 'Widget B', 'Widget C'],
+    'price': [100, 200, 300]
+})
+
+# Второй DataFrame с информацией о продажах
+data2 = pd.DataFrame({
+    'product_id': [1, 2, 4],
+    'quantity_sold': [10, 20, 15]
+})
+
+print('DataFrame1')
+print(data1)
+print('DataFrame2')
+print(data2)
+
+# Объединение по строкам
+concat_rows = pd.concat([data1, data2], ignore_index=True)
+print("\nОбъединение по строкам:")
+print(concat_rows)
+
+# Объединение по столбцам (axis=1)
+concat_columns = pd.concat([data1, data2], axis=1)
+print("\nОбъединение по столбцам:")
+print(concat_columns)
+
+data11 = pd.DataFrame({
+    'product': ['Widget A', 'Widget B'],
+    'price': [100, 200]
+})
+
+data22 = pd.DataFrame({
+    'quantity_sold': [10, 15]
+})
+
+
+
+
