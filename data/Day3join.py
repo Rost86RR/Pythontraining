4a57b01e2b0724aacfdaf8427499f8d71e4b9c81
Rastsislau Rudziankou <rudenkovrostislav1@gmail.com>
2024-11-07 15:43:44 +0100

    data day3 join

--- data/Day3join.py
+++ data/Day3join.py
@@ -18,18 +18,11 @@ print(data1)
 print("\nDataFrame 2:")
 print(data2)
 
-# Объединение по строкам
-concat_rows = pd.concat([data1, data2], ignore_index=True)
-print("\nОбъединение по строкам:")
-print(concat_rows)
+# Установим product_id как индекс
+data1.set_index('product_id', inplace=True)
+data2.set_index('product_id', inplace=True)
 
-# Объединение по столбцам (axis=1)
-concat_columns = pd.concat([data1, data2], axis=1)
-print("\nОбъединение по столбцам:")
-print(concat_columns)
-
-
-
-
-
-
+# Выполним соединение join
+joined_data = data1.join(data2, how='inner')
+print("\nСоединение с использованием join:")
+print(joined_data)
